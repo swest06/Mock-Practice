7ae8944eed7aefe8feb3095184de57ba2fd6d8dd
Sean West <swest06@dcs.bbk.ac.uk>
2018-05-28 20:04:50 +0100

    double-check code

--- Prac3/3/ques3.py
+++ Prac3/3/ques3.py
@@ -1,3 +1,4 @@
 string = str(input("input string: ")).split()
 string.reverse()
 print(" ".join(string))
+
--- Prac3/6/ques6.py
+++ Prac3/6/ques6.py
@@ -2,7 +2,7 @@ import random
 
 # Random 4 digit int to individual string characters in list
 num = list(str((random.randint(1000, 10000))))
-
+print(num)
 fin = False
 while not fin:
     cows = 0
@@ -12,8 +12,8 @@ while not fin:
         # Comparison
         if guess[i] == num[i]:
             cows +=1
-        else:
-            bulls +=1
+        elif guess[i] in set(num):
+            bulls += 1
     print("{} cows, {} bulls".format(cows, bulls))
     if cows == 4:
         fin = True
